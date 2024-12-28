81d507996aa1f16f290f0b33ed79a3df337457d5
eran.zaksh <eranzaksh@gmail.com>
2024-12-28 16:02:30 +0200

    Testing pipeline with new github credentials

--- question5/main.tf
+++ question5/main.tf
@@ -31,8 +31,8 @@ resource "aws_security_group" "leumi-apache-sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    # cidr_blocks = ["91.231.246.50/32"]
+    # cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["91.231.246.50/32"]
   }
 
   egress {
